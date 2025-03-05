05d17a8ae09056c1fea6a63869e5080828840b58
VicSchbt <victoire.schubert@gmail.com>
2025-03-05 12:53:39 +0100

    👷🏻‍♀️ refactoring TipSelector

--- app/src/main/java/com/example/tipcalculator/tip_calculator_screen/components/TipSelector.kt
+++ app/src/main/java/com/example/tipcalculator/tip_calculator_screen/components/TipSelector.kt
@@ -1,9 +1,8 @@
 package com.example.tipcalculator.tip_calculator_screen.components
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
@@ -16,6 +15,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalFocusManager
@@ -42,78 +42,112 @@ fun TipSelector(
     var customTip by remember { mutableStateOf("") }
     val focusManager = LocalFocusManager.current
 
-
     Label("Select Tip %")
 
-    LazyVerticalGrid(
-        columns = GridCells.Fixed(2),
-        horizontalArrangement = Arrangement.spacedBy(16.dp),
-        verticalArrangement = Arrangement.spacedBy(16.dp)
-    ) {
-        tipsOptions.forEach { tip ->
-            item {
-                Button(
+    for (i in 0..tipsOptions.size step 2) {
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            if (i < tipsOptions.size) {
+                val tip = tipsOptions[i]
+                TipButton(
+                    tip,
                     onClick = {
-                        selectedTip = tip  // Update selected tip
-                        customTip = ""  // Reset custom input
-                        focusManager.clearFocus() // Hide keyboard
+                        selectedTip = tip
+                        customTip = ""
+                        focusManager.clearFocus()
                         onTipSelected(tip)
                     },
-                    colors = ButtonDefaults.buttonColors(
-                        contentColor = if (selectedTip == tip) colorResource(R.color.very_dark_cyan) else colorResource(
-                            R.color.white
-                        ),
-                        containerColor = if (selectedTip == tip) colorResource(R.color.strong_cyan) else colorResource(
-                            R.color.very_dark_cyan
-                        )
-                    ),
-                    shape = RoundedCornerShape(8.dp),
-                    modifier = Modifier
-                        .semantics {
-                            role = Role.RadioButton
-                            stateDescription =
-                                if (selectedTip == tip) "Selected" else "Not Selected"
-                        }
-                ) {
-                    Text("${tip.toInt()} %", fontFamily = SpaceMono, fontSize = 20.sp)
-                }
+                    isSelected = selectedTip == tip,
+                    modifier = Modifier.weight(1f)
+                )
             }
-        }
-        item {
-            OutlinedTextField(
-                value = customTip,
-                onValueChange = { newText ->
+            if (i + 1 < tipsOptions.size) {
+                val tip = tipsOptions[i + 1]
+                TipButton(
+                    tip,
+                    onClick = {
+                        selectedTip = tip
+                        customTip = ""
+                        focusManager.clearFocus()
+                        onTipSelected(tip)
+                    },
+                    isSelected = selectedTip == tip,
+                    modifier = Modifier.weight(1f)
+                )
+
+            }
+            if (tipsOptions.size in intArrayOf(i, i + 1)) {
+                TipTextField(
+                    customTip,
+                    onValueChange = { newText: String ->
                     customTip = newText
-                    selectedTip = null  // Unselect buttons
+                        selectedTip = null
                     val tipValue = newText.toFloatOrNull()
                     if (tipValue != null) {
                         onTipSelected(tipValue)
                     }
                 },
-                label = {
-                    Text(
-                        text = "Custom",
-                        textAlign = TextAlign.End,
-                        modifier = Modifier.fillMaxWidth(),
-                        fontFamily = SpaceMono,
-                        color = colorResource(id = R.color.dark_grayish_cyan)
-                    )
-                },
-                shape = RoundedCornerShape(8.dp),
-                textStyle = TextStyle(textAlign = TextAlign.End),
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-                modifier = Modifier
-                    .fillMaxWidth(),
-                colors = OutlinedTextFieldDefaults.colors(
-                    focusedBorderColor = colorResource(R.color.strong_cyan),
-                    unfocusedBorderColor = Color.Transparent,
-                    disabledBorderColor = Color.Transparent,
-                    unfocusedContainerColor = colorResource(id = R.color.very_light_grayish_cyan),
-                    focusedContainerColor = colorResource(id = R.color.very_light_grayish_cyan)
-                ),
-
+                    modifier = Modifier.weight(1f)
                 )
-        }
+            }
 
+        }
     }
+
+}
+
+@Composable
+fun TipButton(tip: Float, isSelected: Boolean, onClick: () -> Unit, modifier: Modifier) {
+    Button(
+        onClick = { onClick() },
+        colors = ButtonDefaults.buttonColors(
+            contentColor = if (isSelected) colorResource(R.color.very_dark_cyan) else colorResource(
+                R.color.white
+            ),
+            containerColor = if (isSelected) colorResource(R.color.strong_cyan) else colorResource(
+                R.color.very_dark_cyan
+            )
+        ),
+        shape = RoundedCornerShape(8.dp),
+        modifier = modifier.semantics {
+            role = Role.RadioButton
+            stateDescription =
+                if (isSelected) "Selected" else "Not Selected"
+        }
+    ) {
+        Text("${tip.toInt()} %", fontFamily = SpaceMono, fontSize = 20.sp)
+    }
+}
+
+@Composable
+fun TipTextField(customTip: String, onValueChange: (String) -> Unit, modifier: Modifier) {
+    OutlinedTextField(
+        value = customTip,
+        onValueChange = { newText -> onValueChange(newText) },
+        label = {
+            Text(
+                text = "Custom",
+                textAlign = TextAlign.End,
+                modifier = Modifier.fillMaxWidth(),
+                fontFamily = SpaceMono,
+                color = colorResource(id = R.color.dark_grayish_cyan)
+            )
+        },
+        shape = RoundedCornerShape(8.dp),
+        textStyle = TextStyle(textAlign = TextAlign.End),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        modifier = modifier
+            .fillMaxWidth(),
+        colors = OutlinedTextFieldDefaults.colors(
+            focusedBorderColor = colorResource(R.color.strong_cyan),
+            unfocusedBorderColor = Color.Transparent,
+            disabledBorderColor = Color.Transparent,
+            unfocusedContainerColor = colorResource(id = R.color.very_light_grayish_cyan),
+            focusedContainerColor = colorResource(id = R.color.very_light_grayish_cyan)
+        ),
+
+        )
 }
--- app/src/main/java/com/example/tipcalculator/utils/Utils.kt
+++ app/src/main/java/com/example/tipcalculator/utils/Utils.kt
@@ -2,6 +2,7 @@ package com.example.tipcalculator.utils
 
 fun calculateTip(bill: Float, tipPercentage: Float, people: Int): Pair<Float, Float> {
     val tip = bill * (tipPercentage / 100)
+    val tipPerPerson = tip / people
     val totalPerPerson = (bill + tip) / people
-    return Pair(tip, totalPerPerson)
+    return Pair(tipPerPerson, totalPerPerson)
 }
